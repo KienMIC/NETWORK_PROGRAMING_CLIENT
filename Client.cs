d8d5e2a49fce572c9ac1d9f30ed662ab712cc9d3
Ngô Trung Kiên <kienhp1hp@gmail.com>
2020-04-14 00:14:00 +0700

    UPDATE FILE

---
+++ Client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+public class Client
+{
+
+    private const int BUFFER_SIZE = 1024;
+    private const int PORT_NUMBER = 9999;
+
+    static ASCIIEncoding encoding = new ASCIIEncoding();
+
+    public static void Main()
+    {
+
+        try
+        {
+            TcpClient client = new TcpClient();
+
+            // 1. connect
+            client.Connect("127.0.0.1", PORT_NUMBER);
+            Stream stream = client.GetStream();
+
+            Console.WriteLine("Connected to Server.");
+            
+            while (true)
+            {
+
+                
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+
+                Console.WriteLine("Please enter the number (0-10) : ");
+
+                // 2. send
+                string str = Console.ReadLine();
+                writer.WriteLine(str);
+                writer.AutoFlush = true;
+
+                // 3. receive
+                str = reader.ReadLine();
+                Console.WriteLine(str);
+                if (str.ToUpper() == "BYE")
+                    break;
+            }                                                                          
+            // 4. close
+            stream.Close();
+            client.Close();
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex);
+        }
+
+        Console.Read();
+    }
+}
